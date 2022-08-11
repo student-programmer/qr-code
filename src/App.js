b57cf329be7d32459a671520234453cce0d9daf3
tazer16179 <tazer@gmail.com>
2022-08-11 14:45:10 +0300

    last commit app is redy

--- src/App.js
+++ src/App.js
@@ -5,39 +5,43 @@ import LiveQrCode from './components/live-qr-code';
 import BasicInput from './ui/basic-input';
 
 function App() {
-  const [qrText, setQrText] = useState('')
-  const [qrCode, setQrCode] = useState('')
+	const [qrText, setQrText] = useState('');
+	const [qrCode, setQrCode] = useState('');
 
+	const generateQrCode = () => {
+		QRCode.toDataURL(
+			qrText,
+			{
+				width: 900,
+				margin: 3,
+			},
+			(err, url) => {
+				if (err) return console.log(err);
+				setQrCode(url);
+			}
+		);
+	};
 
-const generateQrCode = () =>{
-  QRCode.toDataURL(qrText, {
-    width:900,
-    margin:3, 
-  }, (err, url) =>{
-    if(err) return console.log(err)
-    setQrCode(url)
-  })
-}
-
-const handleQrCode = (e) =>{
-  setQrText(e.target.value)
-  generateQrCode()
-}
-  return (
-    <div className="App">
-        <LiveQrCode value={qrText}/>
-        <BasicInput 
-        label='qr kod text' 
-        type='text' 
-        value={qrText} 
-        onChange={handleQrCode} 
-        style={{marginTop:20}}
-        />
-        <br/>
-        <a href={qrCode} download={`${qrText}.png`}>download</a>
-
-    </div>
-  );
+	const handleQrCode = e => {
+		setQrText(e.target.value);
+		generateQrCode();
+	};
+	return (
+		<div className='App'>
+			<LiveQrCode value={qrText} />
+			<BasicInput
+				label='qr kod text'
+				type='text'
+				value={qrText}
+				onChange={handleQrCode}
+				style={{ marginTop: 20 }}
+			/>
+			<br />
+			<a href={qrCode} download={`${qrText}.png`}>
+				download
+			</a>
+		</div>
+	);
 }
 
 export default App;
